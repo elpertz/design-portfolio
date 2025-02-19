08909877b7952bfca8915354567684bc236cdf42
elpertz <83992673+elpertz@users.noreply.github.com>
2025-02-18 22:24:03 -0500

    CTAs and copy text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { DownloadIcon, LinkedinIcon, MessageCircleIcon } from 'lucide-react'
 export default function Home() {
   return (
     <section className="py-24">
@@ -14,10 +16,35 @@ export default function Home() {
             <p className="text-muted-foreground text-sm">Product Designer</p>
           </div>
         </div>
-        <h1 className="text-2xl font-medium">
-          👋 Hey there! I'm Pertz, a designer helping tech companies build kick
-          ass digital products.
-        </h1>
+        <div className="mb-8 flex flex-col gap-4">
+          <h1 className="text-2xl font-medium">
+            👋 Hey there! I'm Pertz, a designer helping tech companies build
+            kick ass digital products.
+          </h1>
+          <p className="text-muted-foreground">
+            Based in Bogotá, Colombia, I love playing with new tools and diving
+            into design challenges. Lately, I've been having fun mixing design
+            with code and AI.
+            <br />
+            <br />I work best when teaming up to solve real problems. My secret
+            sauce? Quick sketches, prototypes, and lots of tweaking until we
+            nail it. Keep it fun, get it done.
+          </p>
+        </div>
+        <div className="flex gap-2">
+          <Button>
+            <MessageCircleIcon className="size-4" />
+            Email me
+          </Button>
+          <Button variant="outline">
+            <LinkedinIcon className="size-4" />
+            LinkedIn
+          </Button>
+          <Button variant="outline">
+            <DownloadIcon className="size-4" />
+            Download
+          </Button>
+        </div>
       </div>
     </section>
   )
