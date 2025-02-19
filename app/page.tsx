43bc6eacfefd2c8736ecad2b9251d0c2ba9563a8
elpertz <83992673+elpertz@users.noreply.github.com>
2025-02-18 22:03:31 -0500

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 export default function Home() {
   return (
@@ -9,5 +9,5 @@ export default function Home() {
         </h1>
       </div>
     </section>
-  );
+  )
 }
